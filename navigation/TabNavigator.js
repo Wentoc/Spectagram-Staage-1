92def0eb4ebc109f200755fa77857807729f58a0
Wentoc <wentoqtech@gmail.com>
2021-08-21 13:27:19 +0530

    Second Commit

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,35 +1,60 @@
 import * as React from 'react';
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Ionicons } from 'react-native-vector-icons/Ionicons';
 import Feed from "../screens/Feed";
 import CreateStory from "../screens/CreateStory";
 
-const Tab = createBottomTabNavigator();
+import { RFValue } from "react-native-responsive-fontsize";
+
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = ()=>{
   return (
-      <Tab.Navigator
+        <Tab.Navigator
+        labeled={false}
+        barStyle={styles.bottomTabStyle}
         screenOptions={({ route }) => ({
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            if (route.name === 'Feed') {
-              iconName = focused
-                ? 'book'
-                : 'book-outline';
-            } else if (route.name === 'CreateStory') {
-              iconName = focused ? 'create' : 'create-outline';
+            if (route.name === "Feed") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Create Story") {
+              iconName = focused ? "add-circle" : "add-circle-outline";
             }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
+            return (
+              <Ionicons
+                name={iconName}
+                size={RFValue(25)}
+                color={color}
+                style={styles.icons}
+              />
+            );
+          }
         })}
+        activeColor={"#ee8249"}
+        inactiveColor={"gray"}
       >
         <Tab.Screen name="Feed" component={Feed} />
-        <Tab.Screen name="CreateStory" component={CreateStory} />
+        <Tab.Screen name="Create Story" component={CreateStory} />
       </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "10%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
 
 export default BottomTabNavigator;
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,18 +1,98 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {
+  Component
+} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  FlatList,
+  Platform,
+  StatusBar
+} from 'react-native';
+
+import {
+  RFValue
+} from 'react-native-responsive-fontsize';
+
+let posts = require("./temp_posts.json");
+
+import PostCard from './PostCard';
 
 export default class Feed extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
+
+  renderItem = ({ item: post }) => {
+    return <PostCard post={post} />;
+  }
+
+  keyExtractor = (item, index) => index.toString();
 
   render() {
     return (
-      <View>
-        <Text> Feed </Text>
+      <View style={styles.container}>
+       <SafeAreaView style={styles.droidSafeArea} />
+      <View style={styles.appTitle}>
+        <View style={styles.appIcon}>
+          <Image
+          source={require("../assets/logo.png")}
+          style={styles.iconImage}
+          />
+        </View>
+
+      <View style={styles.appTitleTextContainer}>
+      <Text style={styles.appTitleText}>Spectagram</Text>
       </View>
-    );
+      </View>
+        <View style={styles.cardContainer}>
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={posts}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </View>
+      );
+      
   }
+  
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.8,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+  },
+  cardContainer: {
+    flex: 0.85
+  }
+});
